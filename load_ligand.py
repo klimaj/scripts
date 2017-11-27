d216f1b49301c46a34e1045e28fad5a4cde51a53
Jason Klima <klimaj@uw.edu>
2017-11-27 00:55:54 -0800

    beautifying

--- load_ligand.py
+++ load_ligand.py
@@ -1,6 +1,4 @@
 import pyrosetta
-from pyrosetta import toolbox
-import numpy as np
 import os
 
 os.chdir('/home/klimaj/temp')
@@ -11,7 +9,6 @@ res_set = pose.conformation().modifiable_residue_type_set_for_conf()
 res_set.read_files_for_base_residue_types( ligand_params )
 pose.conformation().reset_residue_type_set_for_conf( res_set )
 pose = pyrosetta.pose_from_file(filename='final_dump_20170926140149.pdb')
-#pose.assign(pose1)
 
 obj = pyrosetta.rosetta.protocols.rosetta_scripts.XmlObjects.create_from_string("""
 <RESIDUE_SELECTORS>
@@ -25,34 +22,28 @@ obj = pyrosetta.rosetta.protocols.rosetta_scripts.XmlObjects.create_from_string(
 """)
 obj2 = pyrosetta.rosetta.protocols.rosetta_scripts.XmlObjects.create_from_string(""" 
 <ROSETTASCRIPTS>
-<RESIDUE_SELECTORS>
-<ResiduePDBInfoHasLabel name="context" property="CONTEXT"/>
-<Index name="ligand2" resnums="1"/>
-<Chain chains="A" name="chA"/>
-<ResidueName name="ligand" residue_name3="NAD"/>
-<And name="context_ligand" selectors="ligand,context"/>
-<Neighborhood atom_names_for_distance_measure="N6" distance="10.0" include_focus_in_subset="0" name="CN01" selector="ligand"/>
+    <RESIDUE_SELECTORS>
+        <ResiduePDBInfoHasLabel name="context" property="CONTEXT"/>
+        <Index name="ligand2" resnums="1"/>
+        <Chain chains="A" name="chA"/>
+        <ResidueName name="ligand" residue_name3="NAD"/>
+        <And name="context_ligand" selectors="ligand,context"/>
+    <Neighborhood atom_names_for_distance_measure="N6" distance="10.0" include_focus_in_subset="0" name="CN01" selector="ligand"/>
 </RESIDUE_SELECTORS>
 <TASKOPERATIONS>
-<OperateOnResidueSubset name="prevent_repacking" selector="CN01" >
-<PreventRepackingRLT/>
-</OperateOnResidueSubset>
+    <OperateOnResidueSubset name="prevent_repacking" selector="CN01" >
+        <PreventRepackingRLT/>
+    </OperateOnResidueSubset>
 </TASKOPERATIONS>
 <MOVERS>
-<FastDesign name="fd" task_operations="prevent_repacking"/>
+    <FastDesign name="fd" task_operations="prevent_repacking"/>
 </MOVERS>
 <PROTOCOLS>
-<Add mover="fd" />
+    <Add mover="fd" />
 </PROTOCOLS>
 </ROSETTASCRIPTS>
 """)
-#thistask = obj2.get_task_operation("prevent_repacking")
-#tf = pyrosetta.rosetta.core.pack.task.TaskFactory()
-#tf.push_back( thistask )
-#task = tf.create_task_and_apply_taskoperations( pose )
-#print(task)
-#m = obj.get_residue_selector("CN01")
+
 n = obj.get_residue_selector("context_ligand")
 resi = list(pyrosetta.rosetta.core.select.get_residues_from_subset(n.apply(pose)))
-print(resi)
-print('Done!!!')
+print('Done!')
